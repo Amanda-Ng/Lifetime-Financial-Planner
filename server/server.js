3d0e4d0743b408ef720781c0b0c9819ba5c79b7c
tytywang <tyler.wang@stonybrook.edu>
2025-03-12 22:56:51 -0400

    Initial setup files

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const mongodb = "mongodb://127.0.0.1:27017/citrifi-db";
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error with MongoDB connection"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  if (db) {
+    db.close()
+      .then(() => console.log("Database connection closed"))
+      .catch((error) => console.log(error));
+  }
+  console.log("Process terminated");
+});
