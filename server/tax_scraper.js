15b0374aae402caa9487fe8b245dfde5fb5d5c7d
tytywang <tyler.wang@stonybrook.edu>
2025-03-20 14:38:37 -0400

    Federal income tax scraped

--- server/tax_scraper.js
+++ server/tax_scraper.js
@@ -22,28 +22,47 @@ async function run() {
     await page.waitForSelector("table");
 
     const single_federal_income_tax = await page.evaluate(() => {
+        // find heading of table
         const table_head = Array.from(document.querySelectorAll("h2")).find(
             (t) => t.innerHTML.toLowerCase().includes("single")
         );
         if (!table_head) return null;
+
+        // traverse HTML to find sibling element that is the table corresponding to the heading
         let table = table_head.nextElementSibling;
+        let found_table = false;
         while (!table.matches(".table")) {
             table = table.nextElementSibling;
+            if (table.matches(".table")) found_table = true;
         }
+        if (!found_table) return null; // ensure that table was found
+
         return Array.from(table.querySelectorAll("tr")).map((row) =>
-            Array.from(row.querySelectorAll("td, th")).map((cell) =>
+            Array.from(row.querySelectorAll("td")).map((cell) =>
                 cell.innerText.trim()
             )
         );
     });
 
     const married_federal_income_tax = await page.evaluate(() => {
-        const table = Array.from(document.querySelectorAll("h2")).find((t) =>
-            t.innerText.toLowerCase().includes("married filing jointly")
+        const table_head = Array.from(document.querySelectorAll("h4")).find(
+            (t) => t.innerText.toLowerCase().includes("married filing jointly")
         );
-        if (!table) return null;
+        if (!table_head) return null;
+
+        // go back up until overarching parent element (that includes the table) is found
+        let parent = table_head.parentElement;
+        let parent_found = false;
+        while (!parent.querySelector("table")) {
+            parent = parent.parentElement;
+            if (parent.querySelector("table")) parent_found = true;
+        }
+        if (!parent_found) return null;
+
+        // now found the table element with querySelector
+        const table = parent.querySelector("table");
         return Array.from(table.querySelectorAll("tr")).map((row) =>
-            Array.from(row.querySelectorAll("td, th")).map((cell) =>
+            Array.from(row.querySelectorAll("td")).map((cell) =>
                 cell.innerText.trim()
             )
         );
