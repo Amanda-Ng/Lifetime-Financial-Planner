8e18e92a22258a258855d9e93dfc9e48c16e97e0
tytywang <tyler.wang@stonybrook.edu>
2025-03-21 19:07:29 -0400

    Place standard deductions in map format

--- server/tax_scraper.js
+++ server/tax_scraper.js
@@ -12,20 +12,15 @@ async function scrape_federal_income_taxes() {
     });
 
     const page = await browser.newPage();
-    await page.goto(
-        "https://www.irs.gov/filing/federal-income-tax-rates-and-brackets",
-        {
-            waitUntil: "domcontentloaded",
-        }
-    );
+    await page.goto("https://www.irs.gov/filing/federal-income-tax-rates-and-brackets", {
+        waitUntil: "domcontentloaded",
+    });
 
     await page.waitForSelector("table");
 
     const single_federal_income_tax = await page.evaluate(() => {
         // find heading of table
-        const table_head = Array.from(document.querySelectorAll("h2")).find(
-            (t) => t.innerHTML.toLowerCase().includes("single")
-        );
+        const table_head = Array.from(document.querySelectorAll("h2")).find((t) => t.innerHTML.toLowerCase().includes("single"));
         if (!table_head) return null;
 
         // traverse HTML to find sibling element that is the table corresponding to the heading
@@ -37,17 +32,11 @@ async function scrape_federal_income_taxes() {
         }
         if (!found_table) return null; // ensure that table was found
 
-        return Array.from(table.querySelectorAll("tr")).map((row) =>
-            Array.from(row.querySelectorAll("td")).map((cell) =>
-                cell.innerText.trim()
-            )
-        );
+        return Array.from(table.querySelectorAll("tr")).map((row) => Array.from(row.querySelectorAll("td")).map((cell) => cell.innerText.trim()));
     });
 
     const married_federal_income_tax = await page.evaluate(() => {
-        const table_head = Array.from(document.querySelectorAll("h4")).find(
-            (t) => t.innerText.toLowerCase().includes("married filing jointly")
-        );
+        const table_head = Array.from(document.querySelectorAll("h4")).find((t) => t.innerText.toLowerCase().includes("married filing jointly"));
         if (!table_head) return null;
 
         // go back up until overarching parent element (that includes the table) is found
@@ -61,11 +50,7 @@ async function scrape_federal_income_taxes() {
 
         // now found the table element with querySelector
         const table = parent.querySelector("table");
-        return Array.from(table.querySelectorAll("tr")).map((row) =>
-            Array.from(row.querySelectorAll("td")).map((cell) =>
-                cell.innerText.trim()
-            )
-        );
+        return Array.from(table.querySelectorAll("tr")).map((row) => Array.from(row.querySelectorAll("td")).map((cell) => cell.innerText.trim()));
     });
 
     console.log("Single Federal Income Tax", single_federal_income_tax);
@@ -88,13 +73,8 @@ async function scrape_standard_deductions() {
     await page.waitForSelector("table");
 
     const standard_deductions = await page.evaluate(() => {
-        const table_head = Array.from(document.querySelectorAll("p")).find(
-            (t) =>
-                t.innerText
-                    .toLowerCase()
-                    .includes(
-                        "table 10-1.standard deduction chart for most people"
-                    )
+        const table_head = Array.from(document.querySelectorAll("p")).find((t) =>
+            t.innerText.toLowerCase().includes("table 10-1.standard deduction chart for most people")
         );
         if (!table_head) return null;
 
@@ -107,14 +87,22 @@ async function scrape_standard_deductions() {
         if (!div_found) return "not found";
 
         const table = surrounding_div.querySelector("table");
-        return Array.from(table.querySelectorAll("tr")).map((row) =>
-            Array.from(row.querySelectorAll("td")).map((cell) =>
-                cell.innerText.trim()
-            )
-        );
+        return Array.from(table.querySelectorAll("tr")).map((row) => Array.from(row.querySelectorAll("td")).map((cell) => cell.innerText.trim()));
     });
 
-    console.log("Standard Deductions Post 1960", standard_deductions);
+    const standard_deductions_trimmed = standard_deductions.slice(1, standard_deductions.length - 1); // only take middle elements in the table
+
+    const standard_deductions_map = {};
+    Array.from(standard_deductions_trimmed).forEach((element) => {
+        if (element[0].toLowerCase().includes("single")) {
+            console.log("Single Standard Deductions", element);
+            standard_deductions_map["single"] = element[1];
+        } else if (element[0].toLowerCase().includes("married filing jointly")) {
+            console.log("Married Standard Deductions", element);
+            standard_deductions_map["married"] = element[1];
+        }
+    });
+    console.log("Standard Deductions Post 1960", standard_deductions_map);
 
     await browser.close();
 }
@@ -133,29 +121,18 @@ async function scrape_capital_gains_tax() {
     await page.waitForSelector("div");
 
     const capital_gains_taxes = await page.evaluate(() => {
-        const heading = Array.from(document.querySelectorAll("h2")).find((h) =>
-            h.innerText.toLowerCase().includes("capital gains tax rates")
-        );
+        const heading = Array.from(document.querySelectorAll("h2")).find((h) => h.innerText.toLowerCase().includes("capital gains tax rates"));
 
         let items = [];
         let body_contents = heading;
-        while (
-            !body_contents.innerText
-                .toLowerCase()
-                .includes("limit on the deduction and carryover of losses")
-        ) {
+        while (!body_contents.innerText.toLowerCase().includes("limit on the deduction and carryover of losses")) {
             if (body_contents.matches("p")) {
                 // console.log("Tax rate");
                 // console.log(body_contents.innerText.trim());
                 items.push(body_contents.innerText.trim());
-            } else if (
-                body_contents.matches("ul") ||
-                body_contents.matches("ol")
-            ) {
+            } else if (body_contents.matches("ul") || body_contents.matches("ol")) {
                 console.log("Amounts");
-                let amounts = Array.from(
-                    body_contents.querySelectorAll("li")
-                ).map((li) => li.innerText.trim());
+                let amounts = Array.from(body_contents.querySelectorAll("li")).map((li) => li.innerText.trim());
                 items.push(amounts);
             }
             body_contents = body_contents.nextElementSibling;
