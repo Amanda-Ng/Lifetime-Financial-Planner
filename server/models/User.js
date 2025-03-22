3e0df0147c46b810a8db3e329f90724c8deb181c
tytywang <tyler.wang@stonybrook.edu>
2025-03-22 19:58:36 -0400

    Add user dummy script for testing

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const UserSchema = new Schema(
     {
-        _id: { type: mongoose.Schema.Types.ObjectId, required: true },
         username: { type: String, required: true },
         email: { type: String, required: true },
         passwordHash: { type: String, required: true },
