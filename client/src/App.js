03b4c0ebfd3221f8a1be93503ec374013f64cbf3
XiaoranX115 <xiaoranx226@gmail.com>
2025-03-22 04:00:14 -0400

    Added header. Added routes and contents for: profile, scenario, simulation.

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,25 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import InvestmentForm from "./InvestmentForm";
 import "./App.css";
+import Navbar from "./Navbar.js";
+import ProfilePage from "./ProfilePage.js";
+import Scenario from "./Scenario.js";
+import Simulation from "./Simulation.js";
+import Dashboard from "./Dashboard.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar /> 
         <Sidebar />
-        <div className="main-content">
+        <div className="main-content"> 
           <Routes>
             <Route path="/investment" element={<InvestmentForm />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/scenario" element={<Scenario />} />
+            <Route path="/simulation" element={<Simulation />} />
             {/* Add more Routes here */}
           </Routes>
         </div>
