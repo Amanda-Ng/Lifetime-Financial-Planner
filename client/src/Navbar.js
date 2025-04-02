fd1d64710bbe87ffc4bfd04da1a2aa23adb596a0
Tyler Wang <wangtyler820@gmail.com>
2025-04-01 23:24:45 -0400

    Store username in local storage

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Navbar.css";
 
-function Navbar() {
+Navbar.propTypes = {
+    username: PropTypes.func.isRequired,
+};
+
+function Navbar({ username }) {
     return (
         <nav className="navbar">
             <img src="headerIcon.png" alt="headerIcon" id="headerIcon" />
             <p>CitriFi</p>
             <button>
                 <img src="userPfp.png" alt="headerPfp" className="userPfp_small" />
-                <span>Username</span>
+                <span>{username || "Guest"}</span>
             </button>
         </nav>
     );
